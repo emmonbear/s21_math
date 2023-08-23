b4aa969caa0b22ba90f7b1213f7e1ec51db7ae2e
Emmon Beardly <moskaleviluak@icloud.com>
2023-08-23 14:32:42 +0500

    fix_1: Добавить основные define'ы

--- src/s21_math.h
+++ src/s21_math.h
@@ -7,10 +7,10 @@
 
 #define s21_NULL ((void *)0);
 typedef unsigned long s21_size_t;
-#define s21_nan 0.0 / 0.0;
-#define s21_p_inf 1.0 / 0.0; // +infinity
-#define s21_m_inf -1.0 / 0.0; // -infinity
-#define s21_PI 3.141592653589793;
+#define s21_nan 0.0 / 0.0
+#define s21_p_inf 1.0 / 0.0 // +infinity
+#define s21_m_inf -1.0 / 0.0 // -infinity
+#define s21_PI 3.141592653589793
 
 // Вычисляет абсолютное значение целого числа
 int s21_abs(int x);
