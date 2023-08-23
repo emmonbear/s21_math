6f59400c7e63f2d8570f7c2d24fc1ac43e04ab9c
Emmon Beardly <moskaleviluak@icloud.com>
2023-08-23 11:19:59 +0500

    Реализовать функцию int abs(int x)

---
+++ src/CORE/s21_abs.c
@@ -0,0 +1,5 @@
+#include "../s21_math.h"
+
+int abs(int x) {
+    return x = (x < 0) ? x *(-1) : x;
+}
---
+++ src/s21_math.h
@@ -0,0 +1,13 @@
+#ifndef S21_MATH_H
+#define S21_MATH_H
+
+#include <check.h>
+#include <stdio.h>
+
+#define s21_NULL ((void *)0);
+typedef unsigned long s21_size_t;
+
+// Вычисляет абсолютное значение целого числа
+int abs(int x);
+
+#endif //S21_MATH_H
