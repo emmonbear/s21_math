42f60ec2088540617913111d04fe6ea3692fc94f
Emmon Beardly <moskaleviluak@icloud.com>
2023-08-24 15:18:35 +0500

    fabs: Исправить код

--- src/CORE/s21_fabs.c
+++ src/CORE/s21_fabs.c
@@ -1,8 +1,6 @@
 #include "../s21_math.h"
 
 long double s21_fabs(double x) {
-  if (S21_IS_NAN(x)) x = S21_NAN * (-1);
-  else if (x == S21_NULL_NEG)
-    x = (long int)x * (-1);
+  x = (S21_IS_NAN(x)) ? S21_NAN * (-1) : (x == -0.0) ? (long int)x * (-1) : x;
   return (x < 0) ? -x : x;
 }
