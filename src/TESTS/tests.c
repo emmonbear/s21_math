0b19a99c758da502dbf49500c770fd8c1b239f74
Emmon Beardly <moskaleviluak@icloud.com>
2023-08-24 11:14:12 +0500

    Добавить unit-тест для функции s21_ceil

--- src/TESTS/tests.c
+++ src/TESTS/tests.c
@@ -83,34 +83,57 @@ START_TEST(test_fabs) {
 }
 END_TEST
 
-// START_TEST(test_fabs) {
-//   int FAIL = 0;
-//   int SUCCESS = 0;
-//   double number[] = {-1.79769e+308, -1.23456e+200, -1.23456e+100,
-//   -123456.789,
-//                      -12345.6789,   -1234.56789,   -123.456789, -12.3456789,
-//                      -1.23456789,   -0.123456789,  -0.000012345,  0.0,
-//                      0.000012345,   0.123456789,   1.23456789,    12.3456789,
-//                      123.456789,    1234.56789,    12345.6789,    123456.789,
-//                      1.23456e+100,  1.23456e+200,  1.79769e+308, S21_INF_NEG,
-//                      S21_INF_POS,   S21_NAN};
-//   int count = sizeof(number) / sizeof(number[0]);
-//   for (int i = 0; i < count; i++) {
-//     // int result = s21_ceil(number[i]);
-//     int expected = ceil(number[i]);
-//     if (result == expected)
-//       SUCCESS++;
-//     else {
-//       FAIL++;
-//       printf("\033[43m%f\033[0m\n", number[i]);
-//     }
-//     ck_assert_double_eq(result, expected);
-//   }
-//   printf("\033[43mtest_fabs\033[0m\n");
-//   printf("FAIL: %d\n", FAIL);
-//   printf("SUCCESS: %d\n", SUCCESS);
-// }
-// END_TEST
+START_TEST(test_ceil) {
+  int FAIL = 0;
+  int SUCCESS = 0;
+  double number[] = {-1.79769e+308, -1.23456e+200, -1.23456e+100, -123456.789,
+                     -12345.6789,   -1234.56789,   -123.456789,   -12.3456789,
+                     -1.23456789,   -0.123456789,  -0.000012345,  0.0,
+                     0.000012345,   0.123456789,   1.23456789,    12.3456789,
+                     123.456789,    1234.56789,    12345.6789,    123456.789,
+                     1.23456e+100,  1.23456e+200,  1.79769e+308,  S21_INF_NEG,
+                     S21_INF_POS,   S21_NAN};
+  int count = sizeof(number) / sizeof(number[0]);
+  for (int i = 0; i < count; i++) {
+    double expected = ceil(number[i]);
+    double result = s21_ceil(number[i]);
+    if (isnan(number[i])) {
+      if (isnan(result) && isnan(expected))
+        SUCCESS++;
+      else {
+        printf("\033[43mnumber[%d] = %f\033[0m\n", i, number[i]);
+        printf("result: %f\n", result);
+        printf("expected: %f\n", expected);
+        FAIL++;
+      }
+      ck_assert_ldouble_nan(result);
+    } else if (isinf(number[i])) {
+      if (isinf(result) && isinf(expected))
+        SUCCESS++;
+      else {
+        printf("\033[43mnumber[%d] = %f\033[0m\n", i, number[i]);
+        printf("result: %f\n", result);
+        printf("expected: %f\n", expected);
+        FAIL++;
+      }
+      ck_assert_ldouble_infinite(result);
+    } else {
+      if (result == expected)
+        SUCCESS++;
+      else {
+        FAIL++;
+        printf("\033[43mnumber[%d] = %f\033[0m\n", i, number[i]);
+        printf("result: %f\n", result);
+        printf("expected: %f\n", expected);
+      }
+      ck_assert_ldouble_eq_tol(result, expected, 1e-10);
+    }
+  }
+  printf("\033[43mtest_ceil\033[0m\n");
+  printf("FAIL: %d\n", FAIL);
+  printf("SUCCESS: %d\n", SUCCESS);
+}
+END_TEST
 
 Suite *s21_math_suite(void) {
   Suite *suite = suite_create("s21_string");
@@ -122,6 +145,10 @@ Suite *s21_math_suite(void) {
   TCase *tc_fabs = tcase_create("s21_fabs");
   tcase_add_test(tc_abs, test_fabs);
   suite_add_tcase(suite, tc_fabs);
+
+  TCase *tc_ceil = tcase_create("s21_ceil");
+  tcase_add_test(tc_abs, test_ceil);
+  suite_add_tcase(suite, tc_ceil);
 
   return suite;
 }
