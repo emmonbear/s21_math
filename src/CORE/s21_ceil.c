3f29a915a339544368e65b9d097b04bc24017ffc
Emmon Beardly <moskaleviluak@icloud.com>
2023-08-24 13:12:22 +0500

    Исправить работу функции ceil для обработки -0.0

--- src/CORE/s21_ceil.c
+++ src/CORE/s21_ceil.c
@@ -5,13 +5,11 @@ long double s21_ceil(double x) {
     x = S21_NAN;
   else if (x == S21_INF_POS)
     x = S21_INF_POS;
-  else if (x == S21_INF_NEG)
-    x = S21_INF_NEG;
-  else {
-    if (x > 0)
-      x = (long int)x + 1;
-    else
-      x = (long int)x;
-  }
+  else if ((x >= -1) && (x <= -0.0) && (signbit(x)))
+    x = -0.0;
+  else if (x == 0.0)
+    x = 0.0;
+  else
+    x = (x > 0) ? (long int)x + 1 : (long int)x;
   return (long double)x;
 }
