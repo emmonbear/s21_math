5d7c7fd7eb81930786a41daee82cff32865b71c4
Emmon Beardly <moskaleviluak@icloud.com>
2023-08-24 15:26:53 +0500

    ceil: Обработать -inf

--- src/CORE/s21_ceil.c
+++ src/CORE/s21_ceil.c
@@ -1,15 +1,11 @@
 #include "../s21_math.h"
 
 long double s21_ceil(double x) {
-  if (S21_IS_NAN(x))
-    x = S21_NAN;
-  else if (x == S21_INF_POS)
-    x = S21_INF_POS;
-  else if ((x >= -1) && (x <= -0.0) && (signbit(x)))
-    x = -0.0;
-  else if (x == 0.0)
-    x = 0.0;
-  else
-    x = (x > 0) ? (long int)x + 1 : (long int)x;
+  if (S21_IS_NAN(x)) x = S21_NAN;
+  else if (x == S21_INF_POS) x = S21_INF_POS;
+  else if ((x >= -1) && (x <= -0.0) && (signbit(x))) x = -0.0;
+  else if (x == S21_INF_NEG) x = S21_INF_NEG;
+  else if (x == 0.0) x = 0.0;
+  else x = (x > 0) ? (long int)x + 1 : (long int)x;
   return (long double)x;
 }
