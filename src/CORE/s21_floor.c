eb01595976b2e8749a51ef145ed805178ff01fdb
Emmon Beardly <moskaleviluak@icloud.com>
2023-09-04 20:53:28 +0500

    s21_fmod: Обработать краевые ситуации

--- src/CORE/s21_floor.c
+++ src/CORE/s21_floor.c
@@ -1,17 +1,18 @@
 #include "../s21_math.h"
 
 long double s21_floor(double x) {
-    if (S21_IS_NAN(x))
+  if (S21_IS_NAN(x))
     x = S21_NAN;
   else if (x == S21_INF_POS)
     x = S21_INF_POS;
-  else if ((x >= -1) && (x <= -0.0) && (signbit(x)))
-    x = -0.0;
+  else if ((x >= -1) && (x < -0.0) && (signbit(x)))
+    x = -1.0;
   else if (x == S21_INF_NEG)
     x = S21_INF_NEG;
   else if (x == 0.0)
     x = 0.0;
-  else
-    x = (x > 0) ? (long int)x : (long int)x - 1;
+  else {
+    x = (x <= 0) ? ((long long)x) - 1 : (long long)x;
+  }
   return (long double)x;
 }
