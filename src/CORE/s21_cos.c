f549def74e15a1364e1b52a49c5aa47df4c0d564
Emmon Beardly <moskaleviluak@icloud.com>
2023-09-06 22:47:44 +0500

    Реализовать функцию s21_cos

---
+++ src/CORE/s21_cos.c
@@ -0,0 +1,17 @@
+#include "../s21_math.h"
+
+long double s21_cos(double x) {
+    long double result = 0.0;
+    long double temp = x;
+    if ((S21_IS_INF(x)) || (S21_IS_NAN(x))) result = S21_NAN;
+    x = s21_fmod(x, 2 * S21_PI);
+    while (x > S21_PI) x -= 2 * S21_PI;
+    while (x < -S21_PI) x += 2 * S21_PI;
+    int n = 0;
+    while (s21_fabs(temp) > S21_EPS) {
+      temp = ((s21_fast_pow(-1, n) * (s21_fast_pow(x, 2 * n))) / (s21_factorial(2 * n)));
+      n++;
+      result += temp;
+    }
+    return result;
+}
