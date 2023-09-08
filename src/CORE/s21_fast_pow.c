f19ff28b7f2911e5d4f9c1783e5665f768d4fa81
Emmon Beardly <moskaleviluak@icloud.com>
2023-09-08 05:55:27 +0500

    clang_format

--- src/CORE/s21_fast_pow.c
+++ src/CORE/s21_fast_pow.c
@@ -1,15 +1,13 @@
 #include "../s21_math.h"
 
 double s21_fast_pow(double base, int exp) {
-    double result = 1.0;
-    double current = base;
-    int abs_exp = (exp < 0) ? -exp : exp;
-    while (abs_exp > 0) {
-        if (abs_exp % 2 == 1)
-            result *= current;
-        current *= current;
-        abs_exp /= 2;
-    }
-    return result;
-    
+  double result = 1.0;
+  double current = base;
+  int abs_exp = (exp < 0) ? -exp : exp;
+  while (abs_exp > 0) {
+    if (abs_exp % 2 == 1) result *= current;
+    current *= current;
+    abs_exp /= 2;
+  }
+  return result;
 }
--- src/CORE/s21_sin.c
+++ src/CORE/s21_sin.c
@@ -1,17 +1,18 @@
 #include "../s21_math.h"
 
-  long double s21_sin(double x) {
-    long double result = 0.0;
-    long double temp = x;
-    if ((S21_IS_INF(x)) || (S21_IS_NAN(x))) result = S21_NAN;
-    x = s21_fmod(x, 2 * S21_PI);
-    while (x > S21_PI) x -= 2 * S21_PI;
-    while (x < -S21_PI) x += 2 * S21_PI;
-    int n = 0;
-    while (s21_fabs(temp) > S21_EPS) {
-      temp = (s21_fast_pow(-1, n) * s21_fast_pow(x, ((2 * n) + 1))) / (s21_factorial(((2 * n) + 1)));
-      n++;
-      result += temp;
-    }
-    return result;
+long double s21_sin(double x) {
+  long double result = 0.0;
+  long double temp = x;
+  if ((S21_IS_INF(x)) || (S21_IS_NAN(x))) result = S21_NAN;
+  x = s21_fmod(x, 2 * S21_PI);
+  while (x > S21_PI) x -= 2 * S21_PI;
+  while (x < -S21_PI) x += 2 * S21_PI;
+  int n = 0;
+  while (s21_fabs(temp) > S21_EPS) {
+    temp = (s21_fast_pow(-1, n) * s21_fast_pow(x, ((2 * n) + 1))) /
+           (s21_factorial(((2 * n) + 1)));
+    n++;
+    result += temp;
+  }
+  return result;
 }
