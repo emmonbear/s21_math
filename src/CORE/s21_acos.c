9c8af8a4e32ee1f2c186cdebdca59922163bb515
Emmon Beardly <moskaleviluak@icloud.com>
2023-09-10 20:59:52 +0500

    clang-format

--- src/CORE/s21_acos.c
+++ src/CORE/s21_acos.c
@@ -1,5 +1,6 @@
 #include "../s21_math.h"
 
 long double s21_acos(double x) {
-    return S21_PI / 2 - s21_asin(x);
+  if ((S21_IS_INF(x)) || (S21_IS_NAN(x))) return S21_NAN;
+  return S21_PI / 2 - s21_asin(x);
 }
