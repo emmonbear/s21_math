cec5e49a3c2bdf7caf2772b4d82699b3172ed85d
emmonbea <moskaleviluak@icloud.com>
2024-01-22 00:26:19 +0500

    feat: add s21_math.h

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,10 +1,15 @@
+/**
+ * @file s21_math.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief main header
+ * @version 1.0
+ * @date 2024-01-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #ifndef S21_MATH_H_
 #define S21_MATH_H_
-
-
-
-
-
 
 
 #endif // S21_MATH_H_
