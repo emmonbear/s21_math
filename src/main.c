479dd2241d6593834f97333b0cedd3f771c17c78
emmonbea <moskaleviluak@icloud.com>
2024-01-22 00:24:32 +0500

    feat: add debug file

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/**
+ * @file main.c
+ * @author emmonbea (https://github.com/emmonbear)
+ * @brief Debug file
+ * @version 1.0
+ * @date 2024-01-22
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <stdio.h>
+
+/**
+ * @brief Debug file
+ * @author emmonbea
+ * @return int 
+ */
+int main()
+{
+    return 0;
+}
