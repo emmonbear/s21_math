c271add0076d705963a90fbf016df12b20fa75d9
emmonbea <moskaleviluak@icloud.com>
2024-01-23 21:24:21 +0500

    feat: add defines
    add S21_IS_NAN for check NaN
    
    add S21_IS_INF for check inf

--- src/modules/common.h
+++ src/modules/common.h
@@ -16,6 +16,8 @@
 
 #define S21_NAN 0.0 / 0.0                   ///< Value is Not a Number (NaN).
 #define S21_INF 1.0 / 0.0                   ///< Value is infinity (inf).
+#define S21_IS_NAN(x) (x != x)              ///< Check value is Not a Number (NaN)
+#define S21_IS_INF(x) (x == S21_INF)        ///< Check value is infinity (inf)
 #define GET_SHIFT(x) (sizeof(x) * 8 - 1);   ///< Returns the number of bits of the specified type depending on the processor architecture.
 
 #endif // MAIN_COMMON_H_
