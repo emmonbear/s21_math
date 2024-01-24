e5cbab3be4aaa188f62272bb7feb5553507b718a
emmonbea <moskaleviluak@icloud.com>
2024-01-24 22:28:07 +0500

    feat: add module s21_log

---
+++ src/modules/include/s21_log.h
@@ -0,0 +1,18 @@
+/**
+ * @file s21_log.h
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief Header file for s21_log module
+ * @version 1.0
+ * @date 2024-01-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef S21_LOG_H_
+#define S21_LOG_H_
+
+#include "./../common.h"
+
+long double s21_log(double x);
+
+#endif // S21_LOG_H_
---
+++ src/modules/s21_log.c
@@ -0,0 +1,23 @@
+/**
+ * @file s21_log.c
+ * @author emmonbea (moskaleviluak@icloud.com)
+ * @brief Implementation of function log from math.h library
+ * @version 1.0
+ * @date 2024-01-24
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "./include/s21_log.h"
+
+/**
+ * @brief Computes natural logarithm
+ * 
+ * @param x A value used in the calculation of the logarithm of x to the base of e
+ * @return long double 
+ */
+long double s21_log(double x)
+{
+    return x;
+}
