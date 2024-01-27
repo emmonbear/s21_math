67140441cb2eef9ad0f214fd4042f983b2f5b865
emmonbea <moskaleviluak@icloud.com>
2024-01-27 06:22:58 +0500

    feat: add s21_log module

--- src/modules/s21_log.c
+++ src/modules/s21_log.c
@@ -14,10 +14,30 @@
 /**
  * @brief Computes natural logarithm
  * 
+ * @details The function calculates the natural logarithm using Taylor series:
+ * \f[
+ *      ln(x) = 2 \left( \frac{{x - 1}}{{x + 1}} + \frac{1}{3} \left(\frac{{x - 1}}{{x + 1}}\right)^3 + \frac{1}{5} \left(\frac{{x - 1}}{{x + 1}}\right)^5 + \ldots \right)
+ * \f]
  * @param[in] x A value used in the calculation of the logarithm of x to the base of e
- * @return long double 
+ * @return long double
+ * 
+ * 
+ * @todo Изучить синтаксис LaTex
+ * @todo Добавить формулу вычисления log через ряды Тейлора
  */
 long double s21_log(double x)
 {
-    return x;
+    double result;
+    if(!x) {
+        result = S21_INF_NEG;
+    } else if(x < 0 || S21_IS_NAN(x) || x == S21_INF_NEG) {
+        result = S21_NAN;
+    } else if(x == 1.0) {
+        result = 0.0;
+    } else if(x == S21_INF_POS) {
+        result = S21_INF_POS;
+    } else {
+        result = 1.0;
+    }
+    return result;
 }
