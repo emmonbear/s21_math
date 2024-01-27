5a3ae3a7672b669d2f53187bf43ada01a1aaaf12
emmonbea <moskaleviluak@icloud.com>
2024-01-27 05:28:35 +0500

    feat: include ./modules/include/s21_log.h

--- src/s21_math.h
+++ src/s21_math.h
@@ -4,17 +4,18 @@
  * @brief main header
  * @version 1.0
  * @date 2024-01-22
- * 
+ *
  * @copyright Copyright (c) 2024
- * 
+ *
  */
 #ifndef S21_MATH_H_
 #define S21_MATH_H_
 
 #include "./modules/include/s21_abs.h"
-#include "./modules/include/s21_fabs.h"
 #include "./modules/include/s21_ceil.h"
-#include "./modules/include/s21_floor.h"
 #include "./modules/include/s21_exp.h"
+#include "./modules/include/s21_fabs.h"
+#include "./modules/include/s21_floor.h"
+#include "./modules/include/s21_log.h"
 
-#endif // S21_MATH_H_
+#endif  // S21_MATH_H_
