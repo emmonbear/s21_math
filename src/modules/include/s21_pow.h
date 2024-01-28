88dcfe363d1ad4a254829f72a3ee7de80ae97caa
kossadda <gabilov1997@gmail.com>
2024-01-28 06:57:41 +0500

    feat: add module s21_pow

---
+++ src/modules/include/s21_pow.h
@@ -0,0 +1,19 @@
+/**
+ * @file s21_pow.h
+ * @author kossadda (https://github.com/kossadda)
+ * @brief Header file for s21_pow module
+ * @version 1.0
+ * @date 2024-01-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#ifndef S21_POW_H_
+#define S21_POW_H_
+
+#include "./../common.h"
+
+long double s21_pow(double base, double exp);
+
+#endif // S21_POW_H_
---
+++ src/modules/s21_pow.c
@@ -0,0 +1,26 @@
+/**
+ * @file s21_pow.c
+ * @author kossadda (https://github.com/kossadda)
+ * @brief Implementation of function pow from math.h library
+ * @version 1.0
+ * @date 2024-01-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "./include/s21_pow.h"
+
+/**
+ * @brief Raises a number to a given power.
+ * 
+ * @param[in] base number whose absolute value must be determined.
+ * @param[in] exp the degree to which it is raised base.
+ * @return long double - result of calculation.
+ */
+long double s21_pow(double base, double exp)
+{
+    long double result = base = exp;
+
+    return result;
+}
